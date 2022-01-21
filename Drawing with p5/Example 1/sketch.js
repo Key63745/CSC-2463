dbf5d5003b8bb15d4a51a4005084da156ea756fd
Key63745 <jlkey0812@gmail.com>
2022-01-20 22:13:34 -0600

    Update sketch.js

--- Drawing with p5/Example 1/sketch.js
+++ Drawing with p5/Example 1/sketch.js
@@ -1,7 +1,17 @@
 function setup() {
-  createCanvas(400, 400);
+  var cnv = createCanvas(600, 300);
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) /2;
+  cnv.position(x,y);
 }
 
 function draw() {
-  background(90, 58, 150);
+  background(0, 255, 0);
+
+
+  circle(150, 150, 250);
+  square(330, 31, 245);
+  
+  
+
 }
